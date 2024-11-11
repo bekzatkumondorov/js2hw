112096c50a67e226d50443fe346fa1135e65ea62
bekzatkumondorov <barcelona.iliyas@gmail.com>
2024-11-11 20:41:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const f_family = prompt("Enter your fisrt sibling");
+const s_family = prompt("Enter your second sibling")
+const third_family = prompt("Enter your third sibling")
+const fourth_family = prompt("Enter your fourth sibling")
+const fivth_family = prompt("Enter your fifth sibling")
+const sixth_family = prompt("Enter your sixth sibling")
+
+const first_age = prompt("Enter first sibling's age");
+const second_age = prompt("Enter second sibling's age")
+const third_age = prompt("Enter third sibling's age")
+const fourth_age = prompt("Enter fourth sibling's age")
+const fifth_age = prompt("Enter fifth sibling's age")
+const sixth_age = prompt("Enter sixth sibling's age")
+
+
+
+console.log(`1)Your first brother's (sister) name is ${f_family} and his (her) age is ${first_age} 
+    2)Your second brother's (sister) name is ${s_family} and his (her) age is ${second_age}
+    3)Your third brother's (sister) name is ${third_family} and his (her) age is ${third_age}
+    4)Your fourth brother's (sister) name is ${fourth_family} and his (her) age is ${fourth_age}
+    5)Your fifth brother's (sister) name is ${fivth_family} and his (her) age is ${fifth_age}
+    6)Your sixth brother's (sister) name is ${sixth_family} and his (her) age is ${sixth_age}`);
+    
+    alert(`1)Your first brother's (sister) name is ${f_family} and his (her) age is ${first_age} 
+    2)Your second brother's (sister) name is ${s_family} and  his (her) age is ${second_age} 
+    3)Your third brother's (sister) name is ${third_family} and  his (her) age is ${third_age}  
+    4)Your fourth brother's (sister) name is ${fourth_family} and  his (her) age is ${fourth_age} 
+    5)Your fifth brother's (sister) name is ${fivth_family} and  his (her) age is ${fifth_age} 
+    6)Your sixth brother's (sister) name is ${sixth_family} and  his (her) age is ${sixth_age} `);
+        
+        
+        // console.log(`1.Your first brother's (sister) age is ${fifth_age} 
+        //     2.Your second brother's (sister) age is ${second_age}
+        //     3.Your third brother's (sister) age is ${third_age}
+        //     4.Your fourth brother's (sister) age is ${fourth_age}
+        //     5.Your fifth brother's (sister) age is ${fifth_age}
+        //     6.Your sixth brother's (sister) age is ${sixth_age}`);
+        
+        //     alert(`1.Your first brother's (sister) age is ${fifth_age}
+        //         2.Your second brother's (sister) age is ${second_age}
+        //         3.Your third brother's (sister) age is ${third_age}
+        //         4.Your fourht brother's (sister) age is ${fourth_age}
+        //         5.Your fifth brother's (sister) age is ${fifth_age}
+        //         6.Your sixth brother's (sister) age is ${sixth_age}`)
+
+
+
+
+
+
+
